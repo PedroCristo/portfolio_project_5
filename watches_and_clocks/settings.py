2228f2bda0b443213db32c1f5d54350f9a9cb51c
Pedro Cristo <pcristo35@gmail.com>
2022-08-30 14:08:21 +0000

    Add products app to the installed apps

--- watches_and_clocks/settings.py
+++ watches_and_clocks/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'products.context_processors.products_selected',
             ],
         },
     },
