3a785efd59fcd9729a9d3dad423c9502e770be50
Pedro Cristo <pcristo35@gmail.com>
2022-08-31 08:42:57 +0000

    Add view, HTML template and URL path for Products page

---
+++ products/views.py
@@ -0,0 +1,79 @@
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.db.models.functions import Lower
+
+from .models import Product, Category, Gender_category, Product_status
+
+
+# Create your views here.
+
+def all_products(request):
+    """ A view to show all products, including sorting and search queries """
+
+    products = Product.objects.all()
+    query = None
+    categories = None
+    gender_categories = None
+    product_status = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if sortkey == 'gender_category':
+                sortkey = 'gender_category__name'
+            if sortkey == 'Product_status':
+                sortkey = 'Product_status__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+            
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'gender_category' in request.GET:
+            gender_category = request.GET['gender_category'].split(',')
+            products = products.filter(gender_category__name__in=gender_category)
+            gender_categories  = Gender_category.objects.filter(name__in=gender_category)
+
+        if 'product_status' in request.GET:
+            product_status = request.GET['product_status'].split(',')
+            products = products.filter(product_status__name__in=product_status)
+            product_status = Product_status.objects.filter(name__in=product_status)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
+
+    context = {
+        'products': products,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
+        'current_gender_category': gender_categories,
+        'current_product_status': product_status,
+    }
+
+    return render(request, 'products/products.html', context)
+
+
