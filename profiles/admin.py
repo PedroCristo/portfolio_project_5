d4c196313bc28055b7f3fe26185975768cb0d615
Pedro Cristo <pcristo35@gmail.com>
2022-09-01 12:39:21 +0000

    Register User Profile admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'default_phone_number',
+        'default_town_or_city',
+        'default_country',
+    )
+
