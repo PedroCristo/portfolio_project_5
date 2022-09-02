28e1bece1b830c59018eb1ce221bb05e7b1fc00c
Pedro Cristo <pcristo35@gmail.com>
2022-09-02 10:07:39 +0000

    Add URL path for bag app

--- watches_and_clocks/urls.py
+++ watches_and_clocks/urls.py
@@ -18,10 +18,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('', include('home.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls'), name='accounts.urls'),
     path('products/', include('products.urls'), name='products.urls'),
+    path('bag/', include('bag.urls'), name='bag.urls'),
     path('summernote/', include('django_summernote.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
