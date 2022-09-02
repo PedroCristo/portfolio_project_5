3bb89a48230b2678ddd570b158898be1dbe3e8b3
Pedro Cristo <pcristo35@gmail.com>
2022-09-02 09:39:53 +0000

    Add Add context processor to display the service review in the home page

---
+++ reviews/context_processors.py
@@ -0,0 +1,13 @@
+from .models import *
+
+
+def service_reviews(request):
+    """ A view to show customers reviews """
+    service_reviews = Reviews.objects.filter(
+        approved=True).order_by("-timestamp")
+
+    context = {
+        'service_reviews': service_reviews,
+    }
+
+    return context
