7274a587193bbf2452c82a653dbf5822299de455
Pedro Cristo <pcristo35@gmail.com>
2022-09-05 15:07:36 +0000

    Add name and service_rating to the list display admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -5,8 +5,10 @@ from .models import Reviews
 @admin.register(Reviews)
 class ReviewsAdmin(admin.ModelAdmin):
     list_display = (
+        'name',
         'review_name',
         'timestamp',
         'rating',
+        'service_rating',
         'approved',
     )
