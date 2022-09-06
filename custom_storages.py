fcae4146947a3dc4dae716abf24a954767e37dfb
Pedro Cristo <pcristo35@gmail.com>
2022-09-06 11:36:00 +0000

    Add comments to the StaticStorage and MediaStorage classes

--- custom_storages.py
+++ custom_storages.py
@@ -3,8 +3,14 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
+    """
+    Tells Django that in production mode use S3 to store the static files
+    """
     location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
+    """
+    Tells Django that in production mode use S3 to store the media files
+    """
     location = settings.MEDIAFILES_LOCATION
