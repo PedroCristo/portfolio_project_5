a98cd7e615c4a945e83bc89f0e96b881f77125e6
Pedro Cristo <pcristo35@gmail.com>
2022-09-14 13:28:54 +0000

    Set debug to 'DEVELOPMENT' in os.environ

--- watches_and_clocks/settings.py
+++ watches_and_clocks/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = False
+# DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['watches-and-clocks-portfolio-5.herokuapp.com', 'localhost']
 
